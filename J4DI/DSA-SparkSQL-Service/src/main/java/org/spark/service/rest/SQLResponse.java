4a0f509445f88af12058de667f55bcc45f13ab0d
Anamaria Rotariu <anamaria.rotariu.w@gmail.com>
2026-05-12 16:09:28 +0300

    Delete J4DI directory

--- J4DI/DSA-SparkSQL-Service/src/main/java/org/spark/service/rest/SQLResponse.java
+++
@@ -1,9 +0,0 @@
-package org.spark.service.rest;
-
-import com.fasterxml.jackson.annotation.JsonRawValue;
-
-public record SQLResponse(
-        String query,
-        @JsonRawValue
-        String response
-) {}
